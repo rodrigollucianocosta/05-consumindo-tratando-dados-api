07de9a17ea2e9d76587bc767a43ec9ec492e7fe8
Rodrigo.Luciano <brdc.cxlviii1@huawei.com>
2024-01-17 17:41:06 -0300

    update video image update video title update video descricao

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 const containerVideos = document.querySelector(".videos__container");
 
 const api = fetch("http://localhost:3000/videos")
+//test 
+//const api = fetch("http://localhost:3000/video") error
+
 .then(res => res.json())
 .then((videos) =>
     videos.forEach((video)=> {
         containerVideos.innerHTML += `
         <li class="videos__item">
-            <iframe src="${video.url}" title"$video.titulo" frameborder="0" allowfullscreen></iframe>
-              
+            <iframe 
+                width="100%" height="60%" src="${video.url}"
+                title"${video.titulo}"
+                frameborder="0" allow="accelerometer; autoplay;
+                clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture"
+                allowfullscreen>
+             </iframe>
+
+            <div class="descricao-video">
+                <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
+                <h3 class="titulo-video">${video.titulo}</h3>
+                <p class="titulo-canal">${video.descricao}</p>
+
+            </div>
         </li>
         `;
     
     })
 )
+.catch((error) => {
+    containerVideos.innerHTML = `
+    <p> Houve um erro ao carregar os vídeos: ${error}</p>
+    `;
+})
