30ebe2153b37cb5ec9bd1ee8109320143602a780
Rodrigo.Luciano <brdc.cxlviii1@huawei.com>
2024-01-18 11:30:21 -0300

    trabalhando com barra de busca

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos(){
-    const busca = await fetch("http://localhost:3000/videos");
-    //test 
-    //const api = fetch("http://localhost:3000/video") error
-    const videos = await busca.json();
+    try{
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
 
-    // .then(res => res.json())
-    // .then((videos) =>
-        videos.forEach((video)=> {
-            containerVideos.innerHTML += `
-            <li class="videos__item">
-                <iframe 
-                    width="100%" height="60%" src="${video.url}"
-                    title"${video.titulo}"
-                    frameborder="0" allow="accelerometer; autoplay;
-                    clipboard-write; encrypted-media; gyroscope;
-                    picture-in-picture"
-                    allowfullscreen>
-                </iframe>
+            videos.forEach((video)=> {
+                if(video.categoria == ""){
+                    throw new Error('Video não tem categoria');
+                }
+                containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe 
+                        width="100%" height="60%" src="${video.url}"
+                        title"${video.titulo}"
+                        frameborder="0" allow="accelerometer; autoplay;
+                        clipboard-write; encrypted-media; gyroscope;
+                        picture-in-picture"
+                        allowfullscreen>
+                    </iframe>
 
-                <div class="descricao-video">
-                    <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
-                    <h3 class="titulo-video">${video.titulo}</h3>
-                    <p class="titulo-canal">${video.descricao}</p>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
 
-                </div>
-            </li>
-            `;
-        
-        })
-//    )
-//    .catch((error) => {
-//        containerVideos.innerHTML = `
-//        <p> Houve um erro ao carregar os vídeos: ${error}</p>
-//        `;
-//    })
+                    </div>
+                </li>
+                `;
+            })
+    }catch(error){
+        containerVideos.innerHTML = `
+            <p> Houve um erro ao carregar os videos: ${error}</p>
+        `
+    }
 }
 
 buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa(){
+    const videos = document.querySelectorAll(".videos__item");
+
+    if(barraDePesquisa.value != ""){
+        for(let video of videos){
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            if(!titulo.includes(valorFiltro)){
+                video.style.display = "none";
+            }else{
+                video.style.display = "block";
+            }
+        }
+    }else{
+        video.style.display = "block";
+
+    }
+}
